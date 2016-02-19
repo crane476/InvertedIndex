e0e051eb2c8eab7ee6d00e57a5862cdd934df2ea
Crane476 <Crane476@Phantom-PC>
2016-02-19 04:10:15 -0600

    Stemming works Added function headers Formatted output (Still needs work)

--- src/invertedindex/InvertedIndex.java
+++ src/invertedindex/InvertedIndex.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Name: Justin Ridgeway
+ * Course: COSC 4315-001
+ * Date: 2/19/16
+ * Description: This program reads in text from a given directory of files and
+ * uses them to build an inverted index. The inverted index is then written to a
+ * file in a directory specified by the user.
  */
 package invertedindex;
 
@@ -13,6 +16,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -26,25 +30,50 @@ public class InvertedIndex {
     /**
      * @param args the command line arguments
      */
+    
+    /*
+    * Function Name: main()
+    * Arguments: String[] args
+    * Returns: N/A
+    * Description: This method takes two inputs from the user. These inputs
+    * are the input file directory, and stop word list directory respectively.
+    * The file paths for each file in the input directory are then read into an
+    * array. Next, the stop words from the specified stop word file are read
+    * into an array. These two arrays are then passed to the getTokens()
+    * function.
+    */
     public static void main(String[] args) {
         // TODO code application logic here
-        String inDirectory = JOptionPane.showInputDialog(null, "Enter a Directory: ");
+        //DON'T FORGET TO PUT COMMENT HEADERS FOR FUNCTIONS!!!!!!!!!
+        String inDirectory = JOptionPane.showInputDialog(null, "Enter an Input Directory: ");
         File fileNames = new File(inDirectory);
+        //add prompt for stopword file
         File fList[] = fileNames.listFiles();
         ArrayList<String> filePaths = new ArrayList<>();
-        ArrayList<String> fileID = new ArrayList<>();
+        ArrayList<String> fileID = new ArrayList<>(); //change to arrayList for stopwords
         for (int i = 0; i < fList.length; i++) {
             filePaths.add(fList[i].toString());
             fileID.add(fList[i].getName());
         }
-        HashMap<String, HashMap<Integer, Integer>> invertedIndex = getTokens(filePaths); //temporary
+        HashMap<String, HashMap<Integer, Integer>> invertedIndex = getTokens(filePaths); //temporary for debugging purposes
         output(invertedIndex);
     }
 
+    /*
+    * Function Name: getTokens()
+    * Arguments: ArrayList<String>, ArrayList<String>
+    * Returns: N/A
+    * Description: This method iterates through a list of files and reads in
+    * text from each file. Each word is converted to lowercase, checked for 
+    * special characters, and stemmed. If it is a stop word it is removed. Once
+    * the word has been tokenized it is then added to the inverted index. After
+    * the inverted index has been built it is passed to the output function.
+    */
+    
     public static HashMap<String, HashMap<Integer, Integer>> getTokens(ArrayList<String> files) {
         String newToken; //current word being read from file
-        HashMap<String, HashMap<Integer, Integer>> index = new HashMap<>(); //List of tokens which map to termFrequency
-        HashMap<Integer, Integer> termFrequency; //list of document ID's which map to number of occurrences
+        HashMap<String, HashMap<Integer, Integer>> index = new HashMap<>(); //List of tokens which map to posting
+        HashMap<Integer, Integer> posting; //list of document ID's which map to number of occurrences
         for (int i = 0; i < files.size(); i++) { //loop through list of files
             try {
                 Scanner fileIn = new Scanner(new File(files.get(i))); //read in word from file
@@ -56,19 +85,19 @@ public class InvertedIndex {
                         newToken = newToken.replaceAll("[-+.^:,]", ""); //remove all special characters
                         newToken = WordStem(newToken); //reduce word to root form
                         if (index.containsKey(newToken)) {
-                            if (index.get(newToken).containsKey(i)) {
-                                HashMap<Integer, Integer> newPosting = (HashMap)index.get(newToken).clone();
-                                newPosting.put(i, newPosting.get(i) + 1);
-                                index.put(newToken, newPosting);
-                            } else {
-                                HashMap<Integer, Integer> newPosting = (HashMap)index.get(newToken).clone();
+                            if (index.get(newToken).containsKey(i)) { //if term has already been encountered in this document
+                                HashMap<Integer, Integer> newPosting = (HashMap) index.get(newToken).clone(); //shallow copy of posting
+                                newPosting.put(i, newPosting.get(i) + 1); //update number of occurrences
+                                index.put(newToken, newPosting); //overwrite posting with new one
+                            } else { //term has been encountered, but not in current document
+                                HashMap<Integer, Integer> newPosting = (HashMap) index.get(newToken).clone();
                                 newPosting.put(i, 1);
                                 index.put(newToken, newPosting);
                             }
-                        } else {
-                            termFrequency = new HashMap<>();
-                            termFrequency.put(i, 1);
-                            index.put(newToken, termFrequency);
+                        } else { //term has not been encountered yet
+                            posting = new HashMap<>();
+                            posting.put(i, 1);
+                            index.put(newToken, posting);
                         }
                     }
                 }
@@ -81,23 +110,45 @@ public class InvertedIndex {
         return index;
     }
 
+    /*
+    * Function Name: output()
+    * Arguments: HashMap<String, HashMap<Integer, Integer>> map, String string
+    * Returns: N/A
+    * Description: This function iterates through the inverted index and appends 
+    * the output to a formatted string. This string is then written to a file
+    * in a directory specified by the user.
+    */
+    
     public static void output(HashMap<String, HashMap<Integer, Integer>> invertedIndex) {
-        Iterator tokens = invertedIndex.keySet().iterator();
+        System.out.printf("%-15s %20s %n", "Token", "<Frequency, DocID>");
+        System.out.println(" ");
+        TreeMap<String, HashMap<Integer, Integer>> sortedIndex = new TreeMap(invertedIndex); //sort invertedIndex by converting to treeMap
+        Iterator tokens = sortedIndex.keySet().iterator();
         while (tokens.hasNext()) {
-            String token = tokens.next().toString();
-            System.out.println(token);
-            Iterator ID = invertedIndex.get(token).keySet().iterator();
+            String token;
+            String posting = "";
+            token = tokens.next().toString();
+            //System.out.println(token);
+            Iterator ID = sortedIndex.get(token).keySet().iterator();
             while (ID.hasNext()) {
                 int docID = (Integer) ID.next();
-                int frequency = invertedIndex.get(token).get(docID);
-                System.out.println("<" + frequency + ", " + docID + ">");
+                int frequency = sortedIndex.get(token).get(docID);
+                if(sortedIndex.get(token).size() < 2){
+                    posting += "<" + frequency + ", " + docID + ">";
+                } else {
+                    posting += "<" + frequency + ", " + docID + ">\n";
+                }
+                //System.out.println("\t\t<" + frequency + ", " + docID + ">");
             }
-            System.out.println(" ");
+            System.out.printf("%-15s %20s %n", token, posting);
+            //System.out.print(token + "\t\t" + posting);
+            //System.out.println(" ");
         }
     }
 
+    //remove method.
     public static String removeStopWords(String word) {
-        List<String> stopWords = Arrays.asList("a", "About", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are",
+        List<String> stopWords = Arrays.asList("a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are",
                 "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both",
                 "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't",
                 "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't",
@@ -119,6 +170,12 @@ public class InvertedIndex {
         return word;
     }
 
+    /*
+    * Function Name: WordStem
+    * Arguments: String string
+    * Returns: String
+    * Description: This method takes a string and reduces it to its root form.
+    */
     public static String WordStem(String word) {
         Stemmer st = new Stemmer();
         String s1 = st.step1(word);
@@ -128,6 +185,5 @@ public class InvertedIndex {
         String s5 = st.step5(s4);
         word = s5;
         return word;
-        //return word;
     }
 }
